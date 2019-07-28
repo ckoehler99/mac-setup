f4b62b5dc1ad2b2ba87759a300a2a5aace22263b
Christian Köhler <ckoehler99@gmail.com>
2019-07-28 18:00:11 +0200

    Update brewConfig.sh

--- brewConfig.sh
+++ brewConfig.sh
@@ -9,6 +9,23 @@ if ! [ -x "$(command -v brew)" ]; then
    fi
 
 brew install git
+brew install ansible
+brew install consul
+brew inatall jq
+brew install vultr
+brew install awscli
+brew install doctl
+brew install scw
+brew install kubernetes-cli
+brew install kubernetes-helm
+brew install wireguard-go
+brew install wireguard-tools
+brew install redis
+brew install restic
+brew install python
+brew install couchdb
+brew install cloc
+brew install zsh
 brew install heroku/brew/heroku
 brew install nmap
 brew install tcpflow
